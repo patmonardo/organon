bbf887dc71e32bf9977a216517d48f98d19c3026
Pat Monardo <patmonardo@gmail.com>
2026-06-02 10:16:08 -0700

    Reality cleanup

--- reality/src/descriptors/procedure/mod.rs
+++
@@ -1,6 +0,0 @@
-mod procedure_descriptor;
-
-pub use procedure_descriptor::{
-    ProcedureCategory, ProcedureConfigFormat, ProcedureDescriptor, ProcedureFacadeDescriptor,
-    ProcedureMode, ProcedureParameterDescriptor, ProcedureParameterKind, ProcedureReturnFormat,
-};
